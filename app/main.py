91c8da63d7bb7e42361ef3edaec7406c3fb07825
IsaacA2151 <46939800+IsaacA2151@users.noreply.github.com>
2019-12-08 20:56:10 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ class GOL:
         pass
     
     def randomGrid(self):
-        self.grid = np.random.choice([0,255],10*10,p=[0.9,0.1]).reshape(10,10)  # Creates a random 4*4 grid with 0 or 255 value, 
+        self.grid = np.random.choice([0,255],10*10,p=[0.5,0.5]).reshape(10,10)  # Creates a random 4*4 grid with 0 or 255 value, 
         #0 being off and 255 being on, p is probability off either colour
         
         plt.imshow(self.grid)  # Displays
@@ -18,6 +18,9 @@ class GOL:
     def applyRules(self):
         pass
     
+    def checkRules(self):
+        pass
+
 test = GOL()
 test.randomGrid()
 print(test.grid)
