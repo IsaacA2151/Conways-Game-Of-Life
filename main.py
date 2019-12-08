4cd058b699fd9553c8487040a3cb4fdb0854f8a7
IsaacA2151 <46939800+IsaacA2151@users.noreply.github.com>
2019-12-08 20:25:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class GOL:
     
     def randomGrid(self):
         self.x = np.random.choice([0,255],4*4,p=[0.5,0.5]).reshape(4,4)  # Creates a random 4*4 grid with 0 or 255 value, 
-        #0 being off and 255 being on, p is probabiliy or either colour
+        #0 being off and 255 being on, p is probability off either colour
         
         plt.imshow(self.x)  # Displays
         
