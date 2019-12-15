fafaa8037344b612058d8eeaecacc04e014f5eac
IsaacA2151 <46939800+IsaacA2151@users.noreply.github.com>
2019-12-15 21:12:43 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,39 +2,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-size = 20
+size = 50
+
 
 class GOL:
     def __init__(self):
         self.surrounding = 0  # Keeps track of surrouding alive cells
         self.toLive = []
         self.toDie = []
+        
 
     def randomGrid(self):
         # Creates a random size*size grid with 0 or 255 value,
         self.grid = np.random.choice(
-            [0, 255], size*size, p=[0.1, 0.9]).reshape(size, size)
+            [0,255], size*size, p=[0.01,0.99]).reshape(size, size)
         # 0 being off and 255 being on, p is probability off either colour
 
     def run(self):
         self.randomGrid()
         
         while True:
+            del self.toLive[:]
+            del self.toDie[:]
             # uncomment next line to see the grid updating
             self.showGrid()
             self.checkRules()
             self.applyRules()
-            
-
+            print(self.grid)
 
     def showGrid(self):
+        plt.figure(figsize=(10,10))
         plt.imshow(self.grid)
         plt.pause(0.1)
         
     def applyRules(self):
         for i in range(len(self.toLive)):
             self.grid[self.toLive[i][0]][self.toLive[i][1]] = 0
-            
+             
         for i in range(len(self.toDie)):
             self.grid[self.toDie[i][0]][self.toDie[i][1]] = 255
             
@@ -81,15 +85,15 @@ class GOL:
                     self.toDie.append([y,x])
                     #self.grid[y][x] = 255
                 
-                elif self.surrounding == 2 or self.surrounding == 3 and self.grid[y][x] == 0 and [y,x] not in self.toLive:
+                if self.surrounding == 2 or self.surrounding == 3 and self.grid[y][x] == 0 and [y,x] not in self.toLive:
                     self.toLive.append([y,x])
                     #self.grid[y][x] = 0
                     
-                elif self.surrounding > 3 and self.grid[y][x] == 0 and [y,x] not in self.toDie:
+                if self.surrounding > 3 and self.grid[y][x] == 0 and [y,x] not in self.toDie:
                     self.toDie.append([y,x])   
                     #self.grid[y][x] = 255                    
                 
-                elif self.surrounding == 3 and self.grid[y][x] == 255 and [y,x] not in self.toLive:
+                if self.surrounding == 3 and self.grid[y][x] == 255:
                     self.toLive.append([y,x])
                     #self.grid[y][x] = 0
 
